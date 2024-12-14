2ef384bd0d27de880d6901ff471341fac114f7db
M Khanfar <m0569760745@gmail.com>
2024-12-14 19:55:19 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo Install rpitx - some package need internet connection -
 
@@ -7,21 +7,21 @@ sudo apt-get install -y libsndfile1-dev git
 sudo apt-get install -y imagemagick libfftw3-dev libraspberrypi-dev
 #For rtl-sdr use
 sudo apt-get install -y rtl-sdr buffer
-# We use CSDR as a dsp for analogs modes thanks to HA7ILM
-git clone https://github.com/F5OEO/csdr
-cd csdr || exit
+# We use CSDR-Plus as a dsp for analogs modes 
+git clone https://github.com/khanfar/CSDR-Plus
+cd CSDR-Plus || exit
 make && sudo make install
 cd ../ || exit
 
 cd src || exit
-git clone https://github.com/F5OEO/librpitx
-cd librpitx/src || exit
+git clone https://github.com/khanfar/librpitx-Plus
+cd librpitx-Plus/src || exit
 make && sudo make install
 cd ../../ || exit
 
 cd pift8
-git clone https://github.com/F5OEO/ft8_lib
-cd ft8_lib
+git clone https://github.com/khanfar/ft8_lib_Plus
+cd ft8_lib_Plus
 make && sudo make install
 cd ../
 make
@@ -32,7 +32,7 @@ sudo make install
 cd .. || exit
 
 printf "\n\n"
-printf "In order to run properly, rpitx need to modify /boot/config.txt. Are you sure (y/n) "
+printf "In order to run properly, Khanfar-PI-TX-PC need to modify /boot/config.txt. Are you sure (y/n) "
 read -r CONT
 
 if [ "$CONT" = "y" ]; then
@@ -51,7 +51,7 @@ if [ "$CONT" = "y" ]; then
    grep -qF "$LINE" "$FILE"  || echo "$LINE" | sudo tee --append "$FILE"
    echo "Installation completed !"
 else
-  echo "Warning : Rpitx should be instable and stop from transmitting !";
+  echo "Warning : Khanfar-PI-TX-PC should be instable and stop from transmitting !";
 fi
 
 
